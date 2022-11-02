7a814abe6fe653c74f099b4fe0a276255d1c3b1a
Marta <maardiakiv@gmail.com>
2022-11-02 19:36:08 +0100

    two tasks completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryNode = document.querySelector(".gallery");
+
+function addImageCard({ description, preview, original } = {}) {
+  galleryNode.innerHTML += `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+galleryItems.forEach((item) => addImageCard(item));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
